993a38dac832211b2486473d4f5e080cc7fc60b0
KjetilOverby <kjeoever@msn.com>
2026-01-25 19:56:41 +0100

    operator fix

--- src/server/api/routers/sawBlade.ts
+++ src/server/api/routers/sawBlade.ts
@@ -75,7 +75,7 @@ export const sawBladeRouter = createTRPCRouter({
           artikkel: input.artikkel?.trim() ?? null,
           side: input.side ?? null,
           note: input.note?.trim() ?? null,
-          createdById: userId || "defaultUserId", // Replace "defaultUserId" with an appropriate default value
+          createdById: userId ?? "defaultUserId", // Replace "defaultUserId" with an appropriate default value
         },
       });
     }),
